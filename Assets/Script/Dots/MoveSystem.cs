7a67ada5152106bcf17841b302fc58ebc08e4427
Yusuf <121219831+Gadaffi508@users.noreply.github.com>
2025-02-28 17:45:22 +0300

    update

---
+++ Assets/Script/Dots/MoveSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public partial struct MoveSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        float deltaTime = SystemAPI.Time.DeltaTime;
+
+        foreach(var (transform, speed) in
+            SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeedComponent>>())
+        {
+            transform.ValueRW.Position += new float3(0, 0, speed.ValueRO.speed * deltaTime);
+
+        }
+    }
+}
