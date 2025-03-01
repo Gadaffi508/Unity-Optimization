c55704c41b956d445e805c541385683b4c112899
Yusuf <121219831+Gadaffi508@users.noreply.github.com>
2025-03-01 11:15:59 +0300

    Attribute added

---
+++ Assets/Script/Attribute/PozitifDegerDrawer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(PositiveValueAttribute))]
+public class PozitifDegerDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property.propertyType == SerializedPropertyType.Integer || property.propertyType == SerializedPropertyType.Float)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            property.intValue = Mathf.Max(0, property.intValue);
+            EditorGUI.PropertyField(position, property, label);
+            EditorGUI.EndProperty();
+        }
+        else
+        {
+            EditorGUI.LabelField(position, label.text, "Bu özellik sadece sayısal değişkenlerde kullanılabilir!");
+        }
+    }
+}
